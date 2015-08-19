a20b04917b4738e309efdb8249112cf91be217fc
Infocatcher <infocatcher.dev.null@gmail.com>
2015-08-19 11:48:20 +0300

    Wait for menu hiding, v. 0.1.1 - 2015-08-19

--- Link/saveAs.js
+++ Link/saveAs.js
@@ -2,19 +2,19 @@
 // Code for link-like types in Handy Clicks extension
 // Calls built-in "Save Link As…" from page context menu
 // (c) Infocatcher 2015
-// version 0.1.0 - 2015-08-16
+// version 0.1.1 - 2015-08-19
 
 var _this = this;
-window.addEventListener("popupshowing", function onPopup(e) {
-	window.removeEventListener(e.type, onPopup, true);
+window.addEventListener("popupshowing", function onPopupShowing(e) {
+	window.removeEventListener(e.type, onPopupShowing, true);
 	var cm = e.target;
 	cm.collapsed = true;
 	setTimeout(function() { // Wait for menu initialization
-		setTimeout(function() {
-			cm.hidePopup();
-			cm.collapsed = false;
-		}, 0);
 		_this.ui.blinkNode();
+		cm.addEventListener("popuphiding", function onPopupHiding(e) {
+			cm.removeEventListener(e.type, onPopupHiding, true);
+			cm.collapsed = false;
+		}, true);
 		var mi = document.getElementById("context-savelink"); // Or id of any other menu item
 		mi.doCommand();
 	}, 0);
